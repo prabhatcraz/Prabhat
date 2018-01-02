d4022971619b01ae2a8b76a9dab46d86db2cd877
Prabhat Ranjan <prabhatr@amazon.com>
2018-01-02 20:06:50 +0100

    Revert "Better UI"
    This reverts commit 6f82dd69bbc4aa676324cc6ffefd9f6215c23bfc.

--- js/script.js
+++ js/script.js
@@ -46,50 +46,11 @@ ResumeBuilder.prototype = {
             $(".experience").append(tileHtml);
         });
 
-        $("#printit").click(function() {
-            window.print()
-        });
-        $("#close").click(function() {
-            $("#container").html("");
-        })
     }
 };
 
 
 $(document).ready(function() {
-    window.editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-        lineNumbers: true,
-        matchBrackets: true,
-        mode: "application/ld+json",
-        continueComments: "Enter",
-        extraKeys: { "Ctrl-Q": "toggleComment" }
-    });
-
-    window.editor.setValue(JSON.stringify(resumeData, null, 2));
-
-    function getEditorcontent() {
-
-        try {
-            var resumeData = editor.getValue();
-            resumeData = JSON.parse(resumeData);
-            return resumeData;
-        }catch(err) {
-            alert("Something wrong with your JSON");
-        }
-    }
-
-    $("#genratePdf").click(function(event) {
-        event.preventDefault();
-        var builder = new ResumeBuilder(getEditorcontent());
-        builder.render();
-        window.print();
-        $("#container").html("");
-    });
-
-    $("#generateResume").click(function(event) {
-        event.preventDefault();
-        var builder = new ResumeBuilder(getEditorcontent());
-        builder.render();
-    });
-
+    var builder = new ResumeBuilder(resumeData);
+    builder.render();
 });
